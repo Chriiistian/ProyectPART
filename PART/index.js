806b8f89d55ccc506c41d86b11d637abe46f2d7f
VictorMunozRios <victor.munoz.rios@alumnos.uta.cl>
2022-07-12 16:21:50 -0400

    version2

--- PART/index.js
+++ PART/index.js
@@ -1,11 +1,10 @@
-//ejecutar python
+//ejecutar python3
 
-//import createIframe from "node-iframe";
-function ejecutar(){
+function ejecutar_datamining(){
     
-    // const script = spawn("python", ["/assets/py/Grafico_frec_acc.py"]);
+    // const script = spawn("python3", ["/assets/py/Grafico_frec_acc.py"]);
     const { spawn } = require('child_process');
-    const script = spawn('python', ['assets/py/Grafico_frec_acc.py']);
+    const script = spawn('python3', ['assets/py/Datamining.py']);
     const chunks = [];
     
     // there is a data chunk from the script available to read
@@ -13,14 +12,90 @@ function ejecutar(){
         chunks.push(data)
     });
     
-    // no more data left to read, parse our JSON object
+    // no more data left to read
+    script.stdout.on('end', () => {
+     
+    });  
+ }
+ function ejecutar_mapa6(){   
+    // const script = spawn("python3", ["/assets/py/Grafico_frec_acc.py"]);
+    const { spawn } = require('child_process');
+    const script = spawn('python3', ['assets/py/hr00-06.py']);
+    const chunks = []; 
+    // there is a data chunk from the script available to read
+    script.stdout.on('data', (data) => {
+        chunks.push(data)
+    });    
+    // no more data left to read
+    script.stdout.on('end', () => {   
+    });
+ }
+
+ function ejecutar_mapa7(){
+    
+    // const script = spawn("python3", ["/assets/py/Grafico_frec_acc.py"]);
+    const { spawn } = require('child_process');
+    const script = spawn('python3', ['assets/py/hr06-12.py']);
+    const chunks = [];
+    
+    // there is a data chunk from the script available to read
+    script.stdout.on('data', (data) => {
+        chunks.push(data)
+    });
+    
+    // no more data left to read
     script.stdout.on('end', () => {
      
     });
-    
-     
  }
- ejecutar();
+
+ function ejecutar_mapa8(){
+    
+    // const script = spawn("python3", ["/assets/py/Grafico_frec_acc.py"]);
+    const { spawn } = require('child_process');
+    const script = spawn('python3', ['assets/py/hr12-19.py']);
+    const chunks = [];
+    
+    // there is a data chunk from the script available to read
+    script.stdout.on('data', (data) => {
+        chunks.push(data)
+    });
+    
+    // no more data left to read
+    script.stdout.on('end', () => {
+     
+    });
+
+    
+  
+ }
+
+ function ejecutar_mapa9(){
+    
+    // const script = spawn("python3", ["/assets/py/Grafico_frec_acc.py"]);
+    const { spawn } = require('child_process');
+    const script = spawn('python3', ['assets/py/hr19-00.py']);
+    const chunks = [];
+    
+    // there is a data chunk from the script available to read
+    script.stdout.on('data', (data) => {
+        chunks.push(data)
+    });
+    
+    // no more data left to read
+    script.stdout.on('end', () => {
+     
+    });
+
+    
+  
+ }
+
+ ejecutar_datamining();
+ ejecutar_mapa6();
+ ejecutar_mapa7();
+ ejecutar_mapa8();
+ ejecutar_mapa9();
 
 const express = require('express')
 const app = express ()
@@ -50,11 +125,42 @@ app.get('/header', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'component/header.html'))
     })
 //MAPAS
+
 app.get('/mapa-accidentes', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'mapas/prueba_mapa.html'))
+    response.sendFile(path.resolve(__dirname, 'mapas/Map1.html'))
     })
 
+app.get('/mapa-accidentes2', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map2.html'))
+})
 
+app.get('/mapa-accidentes3', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'mapas/Map3.html'))
+    })
 
+app.get('/mapa-accidentes4', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map4.html'))
+})
+
+app.get('/mapa-accidentes5', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map5.html'))
+})
+////////////////////////
+app.get('/mapa-accidentes6', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map6.html'))
+})
+
+app.get('/mapa-accidentes7', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map7.html'))
+})
+
+app.get('/mapa-accidentes8', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map8.html'))
+})
+
+app.get('/mapa-accidentes9', (request, response) => {
+response.sendFile(path.resolve(__dirname, 'mapas/Map9.html'))
+})
+                
 
 
